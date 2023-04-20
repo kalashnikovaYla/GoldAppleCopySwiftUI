9730ab70a9e020bf28e3b44c0b2766d7afbe60c5
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-20 23:32:11 +0500

    Initial Commit

---
+++ GoldAppleCopySwiftUI/GoldAppleCopySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldAppleCopySwiftUIApp.swift
+//  GoldAppleCopySwiftUI
+//
+//  Created by sss on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoldAppleCopySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
